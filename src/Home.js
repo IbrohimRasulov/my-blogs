fa063b8720c09353e799447d9de174a9cc9290f0
Ibrohim Rasulov <ibrohim05062000@gmail.com>
2022-02-08 20:50:21 +0500

    Add State hook

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,20 @@
 import React from "react";
+import { useState } from "react";
 
 const Home = () => {
+  const [name, setName] = useState('Mario');
+  const [age, setAge] = useState(30);
+
+  const handleClick = () => {
+    setName('Ibrohim');
+    setAge(22);
+  }
+
   return (
     <div className="home">
       <h2>Homepage</h2>
+      <p>{ name } is { age } years old</p>
+      <button onClick={handleClick}>Click me</button>
     </div>
    );
 }
