f173ab7a68c2b58779156339f162c0e37bc1f791
Ibrohim Rasulov <ibrohim05062000@gmail.com>
2022-02-08 21:09:03 +0500

    Add blogs array and output it in the browser

--- src/Home.js
+++ src/Home.js
@@ -2,19 +2,21 @@ import React from "react";
 import { useState } from "react";
 
 const Home = () => {
-  const [name, setName] = useState('Mario');
-  const [age, setAge] = useState(30);
-
-  const handleClick = () => {
-    setName('Ibrohim');
-    setAge(22);
-  }
+  const [blogs, setBlogs] = useState([
+    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+  ])
 
   return (
     <div className="home">
       <h2>Homepage</h2>
-      <p>{ name } is { age } years old</p>
-      <button onClick={handleClick}>Click me</button>
+      {blogs.map((blog) => (
+        <div className="blog-preview" key={blog.id}>
+          <h2>{ blog.title }</h2>
+          <p>Written by { blog.author }</p>
+        </div>
+      ))}
     </div>
    );
 }
