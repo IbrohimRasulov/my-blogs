85b4d5e9e10adab56ee736e125606ed5cb0f5cfa
Ibrohim Rasulov <ibrohim05062000@gmail.com>
2022-02-08 18:17:02 +0500

    Add Home.js and styles

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import './App.css';
 import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <div className="content">
-        <h1>App component</h1>
+        <Home />
       </div>
     </div>
   );
