c0f6007c14af49ae32048a3c4c4d4874be3c748f
Ibrohim Rasulov <ibrohim05062000@gmail.com>
2022-02-08 16:28:14 +0500

    Create Navbar.js component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import React from 'react';
 import './App.css';
+import Navbar from './Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <div className="content">
         <h1>App component</h1>
       </div>
---
+++ src/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <h1>My Blog</h1>
+      <div className="links">
+        <a href="/">Home</a>
+        <a href="/create">New Blog</a>
+      </div>
+    </nav>
+   );
+}
+
+export default Navbar;
