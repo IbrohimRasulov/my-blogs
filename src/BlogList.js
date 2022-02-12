ccaaea397b64b46a5537359e89aa70f42ef1fc6a
Ibrohim Rasulov <ibrohim05062000@gmail.com>
2022-02-13 01:06:31 +0500

    Fix linters (again)

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,20 +1,19 @@
 import React from 'react';
 
-const BlogList = ({ blogs, title, handleDelete }) => {
-  return (
-    <div className="blog-list">
-      <h2>{ title }</h2>
-      {blogs.map((blog) => (
-        <div className="blog-preview" key={blog.id}>
-          <h2>{ blog.title }</h2>
-          <p>
-            Written by { blog.author }
-          </p>
-          <button type='button' onClick={() => handleDelete(blog.id)}>Delete</button>
-        </div>
-      ))}
-    </div>
-  );
-};
+const BlogList = ({ blogs, title, handleDelete }) => (
+  <div className="blog-list">
+    <h2>{ title }</h2>
+    {blogs.map((blog) => (
+      <div className="blog-preview" key={blog.id}>
+        <h2>{ blog.title }</h2>
+        <p>
+          Written by 
+          { blog.author }
+        </p>
+        <button type="button" onClick={() => handleDelete(blog.id)}>Delete</button>
+      </div>
+    ))}
+  </div>
+);
 
 export default BlogList;
