2d44edd831c4c40cd5803ef21967a8978f951bdc
Ibrohim Rasulov <ibrohim05062000@gmail.com>
2022-02-12 23:37:42 +0500

    Fix more linter errors

--- src/Home.js
+++ src/Home.js
@@ -3,23 +3,23 @@ import BlogList from './BlogList';
 
 const Home = () => {
   const [blogs, setBlogs] = useState([{
-      title: 'My new website',
-      body: 'lorem ipsum...',
-      author: 'mario',
-      id: 1
-    },
-    {
-      title: 'Welcome party!',
-      body: 'lorem ipsum...',
-      author: 'yoshi',
-      id: 2
-    },
-    {
-      title: 'Web dev top tips',
-      body: 'lorem ipsum...',
-      author: 'mario',
-      id: 3
-    },
+    title: 'My new website',
+    body: 'lorem ipsum...',
+    author: 'mario',
+    id: 1,
+  },
+  {
+    title: 'Welcome party!',
+    body: 'lorem ipsum...',
+    author: 'yoshi',
+    id: 2,
+  },
+  {
+    title: 'Web dev top tips',
+    body: 'lorem ipsum...',
+    author: 'mario',
+    id: 3,
+  },
   ]);
 
   const handleDelete = (id) => {
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
-const Navbar = () =>
-<nav className="navbar">
-  <h1>My Blog</h1>
-  <div className="links">
-    <a href="/">Home</a>
-    <a href="/create">New Blog</a>
-  </div>
-</nav>
+const Navbar = () => (
+  <nav className="navbar">
+    <h1>My Blog</h1>
+    <div className="links">
+      <a href="/">Home</a>
+      <a href="/create">New Blog</a>
+    </div>
+  </nav>
+);
 
 export default Navbar;
