0293efefd863beb1b84470d0b9e0137feb6e5454
Ibrohim Rasulov <ibrohim05062000@gmail.com>
2022-02-13 01:10:27 +0500

    Remove linters

--- src/BlogList.js
+++ src/BlogList.js
@@ -6,10 +6,7 @@ const BlogList = ({ blogs, title, handleDelete }) => (
     {blogs.map((blog) => (
       <div className="blog-preview" key={blog.id}>
         <h2>{ blog.title }</h2>
-        <p>
-          Written by 
-          { blog.author }
-        </p>
+        <p>Written by { blog.author }</p>
         <button type="button" onClick={() => handleDelete(blog.id)}>Delete</button>
       </div>
     ))}
