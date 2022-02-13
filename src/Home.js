c32786c7acfea8f2bc1a9242ee88e1847fe72e01
Ibrohim Rasulov <ibrohim05062000@gmail.com>
2022-02-14 00:08:22 +0500

    Fetch data from local database using useEffect

--- src/Home.js
+++ src/Home.js
@@ -2,28 +2,21 @@ import React, { useState, useEffect } from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 },
-  ]);
-
-  const [name, setName] = useState('Mario');
-
-  const handleDelete = (id) => {
-    const newBlogs = blogs.filter((blog) => blog.id !== id);
-    setBlogs(newBlogs);
-  };
+  const [blogs, setBlogs] = useState(null);
 
   useEffect(() => {
-    console.log('useEffect ran');
-  }, [name]);
+    const fetchData = async () => {
+      const response = await fetch('http://localhost:8000/blogs');
+      const data = await response.json();
+      setBlogs(data);
+    }
+
+    fetchData();
+  }, []);
 
   return (
     <div className="home">
-      <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
-      <button onClick={() => setName('Ibrohim')}>Click me</button>
-      <p>{ name }</p>
+      {blogs && <BlogList blogs={blogs} title="All Blogs" />}
     </div>
   );
 };
