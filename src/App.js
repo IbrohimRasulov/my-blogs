b97743adb3f177191478e52581713fca7ab80669
Ibrohim Rasulov <ibrohim05062000@gmail.com>
2022-02-14 10:08:04 +0500

    Install React Router version 5

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import Navbar from './Navbar';
 import Home from './Home';
+import { BrowserRaouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <div className="content">
-        <Home />
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Home />
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
