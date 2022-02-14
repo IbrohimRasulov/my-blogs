dfc0b04df00d579466b6869a692cfd09655bc59c
Ibrohim Rasulov <ibrohim05062000@gmail.com>
2022-02-14 05:39:49 +0500

    Print error message to the page when API is wrong

--- src/Home.js
+++ src/Home.js
@@ -4,17 +4,24 @@ import BlogList from './BlogList';
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
   const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await fetch('http://localhost:8000/blogs');
+
+        if (!response.ok) {
+          throw Error;
+        }
+
         const data = await response.json();
         setBlogs(data);
         setIsPending(false);
       }
-      catch(e) {
-        console.log('Failed to fetch data');
+      catch(err) {
+        setError('Could not fetch data from the server');
+        setIsPending(false);
       }
     }
 
@@ -23,6 +30,7 @@ const Home = () => {
 
   return (
     <div className="home">
+      {error && <div>{error}</div>}
       {isPending && <div>Loading...</div>}
       {blogs && <BlogList blogs={blogs} title="All Blogs" />}
     </div>
