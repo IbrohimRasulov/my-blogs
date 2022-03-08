23e4ffffb9019b958acbbfea56a6a1c981e5e202
Ibrohim Rasulov <ibrohim05062000@gmail.com>
2022-03-09 00:47:18 +0500

    Create BlogDetails component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from './Navbar';
 import Home from './Home';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Create from './Create';
+import BlogDetails from './BlogDetails';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/create" element={<Create />} />
+            <Route path="/blogs/:id" element={<BlogDetails />} />
           </Routes>
         </div>
       </div>
---
+++ src/BlogDetails.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const BlogDetails = () => {
+  return (
+    <div className="blog-details">
+      <h2>Blog Details</h2>
+    </div>
+  );
+};
+
+export default BlogDetails;
