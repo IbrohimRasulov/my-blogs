b6d866816b242a3d08dced476a81c2f4593458aa
Ibrohim Rasulov <ibrohim05062000@gmail.com>
2022-03-09 01:35:30 +0500

    Install router to blog details with useParams hook

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
 const BlogDetails = () => {
+  const { id } = useParams();
+
   return (
     <div className="blog-details">
-      <h2>Blog Details</h2>
+      <h2>Blog Details - { id }</h2>
     </div>
   );
 };
