306222e60104eb375ab0925754e6ce1480588eb9
Ibrohim Rasulov <ibrohim05062000@gmail.com>
2022-03-09 19:52:32 +0500

    Show blog details

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import useFetch from './useFetch';
 
 const BlogDetails = () => {
   const { id } = useParams();
+  const { data: blog, isPending, error } = useFetch('http://localhost:8000/blogs/' + id);
 
   return (
     <div className="blog-details">
-      <h2>Blog Details - { id }</h2>
+      {isPending && <div>Loading...</div>}
+      {error && <div>{error}</div>}
+      {blog && (
+        <article>
+          <h2>{blog.title}</h2>
+          <p>Written by {blog.author}</p>
+          <div>{blog.body}</div>
+        </article>
+      )}
     </div>
   );
 };
