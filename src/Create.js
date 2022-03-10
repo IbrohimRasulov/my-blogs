a59fe504f3d3c03fb7e6cf9addc281846668a900
Ibrohim Rasulov <ibrohim05062000@gmail.com>
2022-03-10 14:37:54 +0500

    Add create component

--- src/Create.js
+++ src/Create.js
@@ -1,11 +1,25 @@
 import React from "react";
 
- const  Create = () => {
-   return (
-     <div className="create">
-       <h2>Add a new blog</h2>
-     </div>
-   );
- }
+const Create = () => {
+  return (
+    <div className="create">
+      <h2>Add a new blog</h2>
+      <form>
+        <div>
+          <label>Blog title</label>
+          <input type="text" required />
+        </div>
+        <span></span>
+        <div>
+          <label>blog author</label>
+          <input type="text" required />
+        </div>
+        <label>Blog body</label>
+        <textarea required></textarea>
+        <button>Add Blog</button>
+      </form>
+    </div>
+  );
+}
 
- export default  Create;
+export default Create;
