20976892b761bcef320e9c2ce0ad5ff8664a153c
Ibrohim Rasulov <ibrohim05062000@gmail.com>
2022-03-10 15:26:04 +0500

    Add form value to local database and output in UI

--- src/Create.js
+++ src/Create.js
@@ -1,21 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Create = () => {
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+  const [body, setBody] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const blog = { title, author, body };
+
+    fetch('http://localhost:8000/blogs', {
+      method: 'POST',
+      headers: { "Content-Type": "application/json" },
+      body:JSON.stringify(blog)
+    }).then(() => {
+      console.log('new blog added');
+    })
+  }
+
   return (
     <div className="create">
       <h2>Add a new blog</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div>
           <label>Blog title</label>
-          <input type="text" required />
+          <input
+            type="text"
+            required
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
         </div>
         <span></span>
         <div>
           <label>blog author</label>
-          <input type="text" required />
+          <input
+            type="text"
+            required
+            value={author}
+            onChange={(e) => setAuthor(e.target.value)}
+          />
         </div>
         <label>Blog body</label>
-        <textarea required></textarea>
+        <textarea
+          required
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+        />
         <button>Add Blog</button>
       </form>
     </div>
